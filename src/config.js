2b2f654a166bfe9382e4fed2c7a3e11378173621
Gaurav Shokhanda <gauravshokanda2@gmail.com>
2021-05-29 14:39:35 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyA19-MCXNX39UgYj36sYTwa9OhlCcW2AWQ",
+  authDomain: "videoapp-e75ff.firebaseapp.com",
+  projectId: "videoapp-e75ff",
+  storageBucket: "videoapp-e75ff.appspot.com",
+  messagingSenderId: "405717683615",
+  appId: "1:405717683615:web:4e43a9cef77d1e1a7101aa",
+  measurementId: "G-4DRTJVX0D7"
+};
+
+  export default config
